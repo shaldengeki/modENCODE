2ba8877a6cfd4fa40a6eb4491994523249735d3a
guoc <guoc@dmel.(none)>
2012-05-23 15:51:04 -0500

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ModENCODE::Application.config.secret_token = 'a3acc8dfc2e863aa1820d734fac26a566741bf0122d579e840a870ed164665c96dc8bc9415dd13be8deedcb42434e3eda449e26906456a7325bec4ef5163649b'
