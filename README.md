83c5a630da146a4036bbced4f25f9494aadb22b6
shaldengeki <shaldengeki@gmail.com>
2012-08-07 16:55:02 -0500

    autogenerate docs, custom error pages

--- README.md
+++ README.md
@@ -3,4 +3,4 @@ modENCODE
 
 Rails app to track progress towards sequencing modENCODE transcription factor binding sites.
 
-To get started, edit `database.yml.example` to fit your production parameters, rename it to `database.yml`, and run `bundle install` to install dependencies.
+To get started, edit `database.yml.example` to fit your production parameters, rename it to `database.yml`, and run `bundle install` to install dependencies. After that, simply running a `rake db:migrate` should bring you to operational status.
--- app/helpers/pages_helper.rb
+++ app/helpers/pages_helper.rb
@@ -10,10 +10,4 @@ module PagesHelper
 
     @pieChartData = [(@inactiveFactorCount/@transcriptionFactorCount*100).round(2), (@processingFactorCount/@transcriptionFactorCount*100).round(2), (@finishedFactorCount/@transcriptionFactorCount*100).round(2)]
   end
-
-  def display_tf_latest_status(transcriptionFactor)
-    # statusUpdate = Status.find(max(transcriptionFactor.isoforms.reagents.attempts.statuses.created_at))
-    link_to '#', content_tag(:h5, "") + "
-          Charles Guo started doing this thing to this factor."
-  end
 end
